c58909d84ce3b394b0ee34fb0dedcc7fd8fcb736
Oleg E <evstyunino@gmail.com>
2024-07-26 15:20:56 +0300

    complete registration process

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
 const express = require('express');
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
-const {MongoClient} = require("mongodb");
+const {MongoClient, ObjectId} = require("mongodb");
 const {z, ZodError} = require("zod");
 
 const client = new MongoClient(process.env.MONGODB_URI);
+
 const User = z.object({
     email: z.string().email(),
-    password: z.string().min(6).transform(async val => await bcrypt.hash(val, 10)),
+    password: z.string().min(6),
     repeat_pass: z.string()
 }).strict().refine(data => data.password === data.repeat_pass, {
     message: "Passwords don't match",
     path: ["password"]
 }).refine(async data => {
-    await client.connect();
-    const db = client.db(process.env.MONGODB_NAME);
-    const users = db.collection('users');
-    const cntUsers = await users.countDocuments({email: data.email});
-    await client.close();
+    const cntUsers = await client.db(process.env.MONGODB_NAME)
+        .collection('users')
+        .countDocuments({email: data.email});
     return !cntUsers;
 }, {
     message: "User already exists",
     path: ["email"]
 });
-
-async function insertUser(email, password) {
-    const {email, password} = data;
-    await client.connect();
-    const db = client.db(process.env.MONGODB_NAME);
-    const users = db.collection('users');
-    const result = await users.insertOne({email, password});
-    await client.close();
-    return result;
-}
-
-async function findUser(email) {
-    await client.connect();
-    const db = client.db(process.env.MONGODB_NAME);
-    const users = db.collection('users');
-    const result = await users.findOne({email});
-    await client.close();
-    return result;
-}
 
 const app = express();
 app.set("view engine", "pug");
@@ -61,17 +41,33 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
+app.get('/profile', isAuthenticated, (req, res) => {
+    res.render('profile');
 })
 
-app.post('/signup', async (req, res, next) => {
-    try {
-        const {email, password} = User.parse(req.body);
-        await insertUser(email, password);
+app.get('/signup', (req, res) => {
+    res.render('signup', {errors: {}});
+})
 
-        // TODO: make authentication
-        res.redirect("/profile");
+app.post('/signup', async (req, res) => {
+    try {
+        const {email, password} = await User.parseAsync(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await client
+            .db(process.env.MONGODB_NAME)
+            .collection('users')
+            .insertOne({email, password: hashedPassword});
+
+        console.log(user);
+
+        req.session.regenerate(err => {
+            if (err) throw err;
+            req.session.user = {email, id: user.insertedId.toString()}
+            req.session.save(err => {
+                if (err) throw err;
+                res.redirect("/profile");
+            })
+        })
     } catch (e) {
         if (e instanceof ZodError) {
             return res.render("signup", {errors: e.flatten().fieldErrors});
@@ -82,7 +78,7 @@ app.post('/signup', async (req, res, next) => {
 
 app.get('/signin', async (req, res) => {
     const {email, password} = req.body;
-    const existingUser = await findUser({email});
+    const existingUser = await app.locals.db.collection('users').findOne({email});
     if (bcrypt.compare(existingUser.password, password)) {
 
     }
@@ -96,6 +92,20 @@ app.post('/profile', (req, res) => {
     // тут обработчик формы
 })
 
-app.listen(3000, () => {
-    console.log("Server started at http://localhost:3000");
-});
+client.connect()
+    .then(client => {
+        console.info("MongoDB connected");
+        app.listen(3000, () => {
+            console.log("Server started at http://localhost:3000");
+        });
+    })
+    .catch(console.error);
+
+const cleanup = async event => {
+    console.info("Connection closed!");
+    await client.close();
+    process.exit();
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
